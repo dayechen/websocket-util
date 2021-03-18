daa85a25614ee40d1b153bd855d62f97f89206e3
chend <chend@imajo.top>
2021-03-18 18:50:16 +0800

    消灭重复代码

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,172 @@
 <template>
-  <div>
-    <input type="number" v-model.number="goodsID" />
-    <button @click="sendOfferMessage">发送消息</button>
+  <div class="box">
+    <div class="socket-box" v-for="(item,index) in testData.slice(0,19)" :key="index">
+      <div v-if="index !== 0">
+        <div>用户{{index}}</div>
+        <div class="content">
+          <div v-if="message[index]">
+            <div v-for="(item1,index1) in message[index]" :key="index1" class="item">{{item1}}</div>
+          </div>
+        </div>
+        <div>
+          <input type="number" v-model.number="chessPos.x" />
+          <input type="number" v-model.number="chessPos.y" />
+          <input type="number" v-model.number="chessPos.level" />
+        </div>
+        <div class="buttons">
+          <button @click="startSocket(index)">打开连接</button>
+          <button @click="closeSocket(index)">关闭连接</button>
+          <button @click="match(index)">开始匹配</button>
+          <button @click="placeChess(index)">出牌</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="js">
-import { defineComponent } from "vue";
+<script>
 import socket from "./util/socket.js";
-
-export default{
+export default {
   created() {
-    this.socket = new socket()
-    this.socket.startSocket()
-    this.socket.on("test",res=>{
-      console.log("收到消息",res)
-    })
+    for (let i = 1; i <= 100; i++) {
+      this.testData.push(i);
+    }
+    this.testData.map((_, i) => {
+      this.initSocket(i);
+    });
   },
   data() {
     return {
       goodsID: 0,
-      socket:null
+      socket: null,
+      socketList: [],
+      chessPos: {
+        x: 0,
+        y: 0,
+        level: 1,
+      },
+      testData: [],
+      message: [], // socket收到的消息
+      eventName: [
+        "test",
+        "start",
+        "match",
+        "hurt",
+        "gameOver",
+        "welcome",
+        "close",
+      ],
     };
   },
   methods: {
-    sendOfferMessage(){
-     this.socket.sendMessage({
-        event:"test",
-        params:{
-          "name":"cao"
+    match(i) {
+      this.socketList[i]
+        .echoMessage({
+          event: "match",
+        })
+        .then((res) => {
+          this.pushMsg(i, res);
+        })
+        .catch((res) => {
+          this.pushMsg(i, res);
+        });
+    },
+    placeChess(i) {
+      console.log(i);
+      this.socketList[i]
+        .echoMessage({
+          event: "placeChess",
+          params: {
+            x: this.chessPos.x,
+            y: this.chessPos.y,
+            level: this.chessPos.level,
+          },
+        })
+        .then((res) => {
+          this.pushMsg(i, res);
+        })
+        .catch((res) => {
+          this.pushMsg(i, res);
+        });
+    },
+    closeSocket(i) {
+      this.socketList[i].close();
+      this.pushMsg(i, "连接关闭");
+    },
+    startSocket(i) {
+      this.socketList[i].start();
+    },
+    query(variable) {
+      const query = window.location.search.substring(1);
+      const vars = query.split("&");
+      for (let i = 0; i < vars.length; i++) {
+        const pair = vars[i].split("=");
+        if (pair[0] == variable) {
+          return pair[1];
         }
-      })
+      }
+      return false;
+    },
+    pushMsg(i, msg) {
+      msg = JSON.stringify(msg);
+      if (!this.message[i]) {
+        this.message[i] = [msg];
+      } else {
+        this.message[i].push(msg);
+      }
+    },
+    initSocket(i) {
+      this.socketList[i] = new socket("ws://192.168.2.107:3000/ws?token=" + i);
+
+      this.eventName.forEach((x) => {
+        this.socketList[i].on(x, (res) => {
+          this.pushMsg(i, res);
+        });
+      });
+
+      this.socketList[i].start();
     },
   },
-}
-
-// export default defineComponent({
-//   name: "App",
-
-// });
+};
 </script>
 
-<style>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.socket-box {
+  display: inline-block;
+  border: solid black 1px;
+  margin-right: 5px;
+  margin-top: 5px;
+}
+.content {
+  height: 400px;
+  width: 460px;
+  overflow: scroll;
+  /* background: red; */
+}
+.box {
+  vertical-align: middle;
+}
+.buttons {
+  width: 440PX;
+}
+.buttons > button {
+  display: inline-block;
+}
+.item {
+  padding: 5px;
+  width: 440px;
+  word-break: break-all;
+  word-wrap: break-word;
+  /* overflow: hidden; */
+}
+.item:nth-child(odd) {
+  background-color: rgb(238, 238, 238);
+}
 </style>
--- src/util/socket.js
+++ src/util/socket.js
@@ -3,34 +3,41 @@ export default class {
   messageID = 0 // 消息的唯一标识
   echoEvents = {} // 发送出去的各种消息
   events = {} // 被注册的各种事件
-  startSocket = () => {
-    this.socketTask = new WebSocket("ws://192.168.2.107:3000/ws?token=998")
+  url = ""
+  constructor(url) {
+    this.url = url
+  }
+  start = () => {
+    this.socketTask = new WebSocket(this.url)
     this.socketTask.onmessage = (e) => {
       const { code, messageID, content, event } = JSON.parse(e.data)
       if (event === 'echo') {
-        const event = this.echoEvents[messageID]
-        if (!event) return
-        event[code === 0 ? 'resolve' : 'reject'](content)
+        const eventFunc = this.echoEvents[messageID]
+        if (!eventFunc) return
+        eventFunc[code === 0 ? 'resolve' : 'reject'](content)
         return
       }
-      try {
-        this.events[event](content)
-      } catch {
-        console.error("收到事件", event, '但未定义处理方法')
+
+      const eventFunc = this.events[event]
+      if (eventFunc) {
+        eventFunc(content)
       }
       // 收到消息调用方法
     }
-    this.socketTask.onerror = (e) => {
+    this.socketTask.onerror = e => {
       console.log("socket连接错误", e)
     }
-    this.socketTask.onclose = (e) => {
+    this.socketTask.onclose = e => {
       console.log("连接被关闭", e)
+      if(this.events["close"]){
+        this.events["close"](e)
+      }
     }
   }
   echoMessage = (data) => {
     this.messageID++ // 自增长确保id不会重复
     data.messageID = this.messageID
-    console.log("发送的数据", data)
+    // console.log("发送的数据", data)
     this.socketTask.send(JSON.stringify(data))
 
     return new Promise((resolve, reject) => {
@@ -40,10 +47,13 @@ export default class {
   sendMessage = (data) => {
     this.messageID++ // 自增长确保id不会重复
     data.messageID = this.messageID
-    console.log("发送的数据", data)
+    // console.log("发送的数据", data)
     this.socketTask.send(JSON.stringify(data))
   }
   on(name, callback) {
     this.events[name] = callback
+  }
+  close(){
+    this.socketTask.close()
   }
 }
